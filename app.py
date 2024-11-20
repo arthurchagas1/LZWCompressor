c0429bbaf826bc59a4552c4b4223aa5137b6507e
Lucas Dayrell de Andrade Machado <119823704+lucasdayrell@users.noreply.github.com>
2024-11-19 23:59:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,22 +54,22 @@ def show_implementations():
     - `root`: o nó raiz da árvore
     - `next_code`: o próximo código disponível para ser atribuído a uma nova sequência de caracteres.
     ### Métodos
-    -`insert(self, string, code)`: insere uma sequência de caracteres (string) na trie e associa a sequência com um código (code). Se nós correspondentes não existirem, eles são criados.
-    -`search(self, string)`: Pesquisa se uma sequência de caracteres está inserida na árvore. Se sim, retorna o código associado. Caso contrário, retorna None.
+    - `insert(self, string, code)`: insere uma sequência de caracteres (string) na trie e associa a sequência com um código (code). Se nós correspondentes não existirem, eles são criados.
+    - `search(self, string)`: Pesquisa se uma sequência de caracteres está inserida na árvore. Se sim, retorna o código associado. Caso contrário, retorna None.
     Ambos os métodos possuem complexidade $O(m)$, onde $m$ é o comprimento da sequência de caracteres. Isso ocorre porque, em cada operação, o código percorre cada caractere da sequência por vez.
     ## LZW Padrão
     O compressor LZW é o componente responsável por comprimir dados da entrada utilizando a trie
     ### Classe LZWCompressor
-    -`__init__(self, max_bits=12)` (construtor): inicializa uma instância de compressor, configurando o número máximo de bits para o código, criando uma trie e inserindo os caracteres ASCII nela com seus respectivos códigos.
-    -`compress(self, input_data)`: Realiza a compressão do texto fornecido. Para cada caractere da entrada: (1): Tenta combiná-lo com o restante da sequência já processada; (2):Se a sequência combinada já estiver na trie, o caractere é adicionado à sequência atual. (3): Caso contrário, o código da sequência existente é armazenado no resultado, e a sequência combinada é adicionada à trie com um novo código; (4): No final, o último código da sequência é adicionado ao resultado.
+    - `__init__(self, max_bits=12)` (construtor): inicializa uma instância de compressor, configurando o número máximo de bits para o código, criando uma trie e inserindo os caracteres ASCII nela com seus respectivos códigos.
+    - `compress(self, input_data)`: Realiza a compressão do texto fornecido. Para cada caractere da entrada: (1): Tenta combiná-lo com o restante da sequência já processada; (2):Se a sequência combinada já estiver na trie, o caractere é adicionado à sequência atual. (3): Caso contrário, o código da sequência existente é armazenado no resultado, e a sequência combinada é adicionada à trie com um novo código; (4): No final, o último código da sequência é adicionado ao resultado.
     A compressão possui complexidade $O(m * n)$, onde $n$ é o número de caracteres da entrada e $m$ é o comprimento da sequência inserida ou pesquisada na trie. Esse comportamento assintótico decorre da necessidade de verificar ou inserir sequências na trie, que é uma operação de tempo linear com relação ao comprimento da sequência.          
     ### Classe LZWDecompressor
-    -`__init__(self, max_bits=12)` (construtor): inicializa o descompressor, criando um dicionário com os primeiros 256 códigos ASCII.
-    -`decompress(self, compressed_data)`: Descomprime os dados. Para cada código: (1): Recupera a sequência correspondente ao código; (2):Se o código é o próximo disponível, a sequência é formada pelo último caractere da sequência anterior repetido; (3):Insere novas sequências no dicionário à medida que o processo avança.
+    - `__init__(self, max_bits=12)` (construtor): inicializa o descompressor, criando um dicionário com os primeiros 256 códigos ASCII.
+    - `decompress(self, compressed_data)`: Descomprime os dados. Para cada código: (1): Recupera a sequência correspondente ao código; (2):Se o código é o próximo disponível, a sequência é formada pelo último caractere da sequência anterior repetido; (3):Insere novas sequências no dicionário à medida que o processo avança.
     A descompressão possui a mesma complexidade assintótica da compressão, pois envolve a reconstrução das sequências a partir de código e as constantes inserções de novos pares no dicionário.
     ## Métodos de leitura e escrita de arquivos
-    -`write_compressed_file(output_path, compressed_data)`: Grava os códigos comprimidos em um arquivo binário, usando 2 bytes por código.
-    -`read_compressed_file(input_path)`: Lê os códigos comprimidos de um arquivo binário e os retorna como uma lista de inteiros.
+    - `write_compressed_file(output_path, compressed_data)`: Grava os códigos comprimidos em um arquivo binário, usando 2 bytes por código.
+    - `read_compressed_file(input_path)`: Lê os códigos comprimidos de um arquivo binário e os retorna como uma lista de inteiros.
     Ambos possuem complexidade $O(n)$, com $n$ sendo o número de códigos, evidentemente. Isso se deve ao fato dos métodos iterarem sobre os códigos.
                 
     ## LZW Dinâmico
@@ -78,7 +78,7 @@ def show_implementations():
     - `compress(self, input_data)`: Realiza a compressão do texto de entrada, com os seguintes passos:(1)Percorre os caracteres da entrada e tenta expandir a sequência (current_string) com o próximo caractere (char).(2)Se a sequência já existe no dicionário, continua a expandir a sequência.(3)Caso contrário, armazena o código da sequência atual no resultado e insere a nova sequência no dicionário.(4)Se o dicionário atinge o limite de códigos (determinado pelo número de bits), o número de bits usados para os códigos é aumentado, ajustando a variável current_bits. (5)Ao final, o código adiciona o código da última sequência ao resultado.
     ### Classe LZWDecompressorDynamic
     - `__init__(self, max_bits=12)` (construtor): Inicializa o compressor. Os parâmetros e fucnionamento da inicialização são análogos ao do compressor.
-    -`decompress(self, input_data)`: Descomprime a entrada da seguinte forma: (1)Recupera a sequência correspondente ao primeiro código da entrada; (2)Para cada código subsequente: Se o código estiver no dicionário, recupera a sequência correspondente; Se o código for igual ao próximo código esperado, a sequência é formada pelo caractere repetido da sequência anterior; Caso contrário, lança um erro; (3)Insere as novas sequências no dicionário à medida que são processadas; (4)Se o número de códigos exceder o limite, o número de bits usados para os códigos é incrementado; (5)O resultado final é reconstruído e retornado como uma string;
+    - `decompress(self, input_data)`: Descomprime a entrada da seguinte forma: (1)Recupera a sequência correspondente ao primeiro código da entrada; (2)Para cada código subsequente: Se o código estiver no dicionário, recupera a sequência correspondente; Se o código for igual ao próximo código esperado, a sequência é formada pelo caractere repetido da sequência anterior; Caso contrário, lança um erro; (3)Insere as novas sequências no dicionário à medida que são processadas; (4)Se o número de códigos exceder o limite, o número de bits usados para os códigos é incrementado; (5)O resultado final é reconstruído e retornado como uma string;
         
     ### Funções de leitura e escrita: 
     São análogas às anteriores. `write_compressed_file(output_path, compressed_data, max_bits)` Grava os dados comprimidos em um arquivo binário, salvando também o número de bits usado para os códigos. `read_compressed_file(input_path)` faz o mesmo do anterior, levando em conta o número de bits utilizados.
